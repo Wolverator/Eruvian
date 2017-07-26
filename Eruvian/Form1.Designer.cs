297dfd6fbc7c0dcba6da11847aeb3ad4522246b3
Wolverator <andrey131295@yandex.ru>
2017-07-26 15:28:25 +0300

    начало интерфейса

--- Eruvian/Form1.Designer.cs
+++ Eruvian/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Eruvian
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,12 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.LogTextBox = new System.Windows.Forms.RichTextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.StorageButton = new System.Windows.Forms.Button();
+            this.GoldLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LogTextBox.Location = new System.Drawing.Point(244, 12);
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.Size = new System.Drawing.Size(324, 333);
+            this.LogTextBox.TabIndex = 0;
+            this.LogTextBox.Text = "";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartButton.Location = new System.Drawing.Point(12, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(226, 34);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PauseButton.Location = new System.Drawing.Point(12, 52);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(226, 34);
+            this.PauseButton.TabIndex = 2;
+            this.PauseButton.Text = "Pause";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            // 
+            // StopButton
+            // 
+            this.StopButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StopButton.Location = new System.Drawing.Point(12, 92);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(226, 34);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            // 
+            // StorageButton
+            // 
+            this.StorageButton.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StorageButton.Location = new System.Drawing.Point(12, 311);
+            this.StorageButton.Name = "StorageButton";
+            this.StorageButton.Size = new System.Drawing.Size(226, 34);
+            this.StorageButton.TabIndex = 4;
+            this.StorageButton.Text = "Storage";
+            this.StorageButton.UseVisualStyleBackColor = true;
+            // 
+            // GoldLabel
+            // 
+            this.GoldLabel.AutoSize = true;
+            this.GoldLabel.Location = new System.Drawing.Point(8, 286);
+            this.GoldLabel.Name = "GoldLabel";
+            this.GoldLabel.Size = new System.Drawing.Size(48, 22);
+            this.GoldLabel.TabIndex = 5;
+            this.GoldLabel.Text = "Gold:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(580, 357);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GoldLabel);
+            this.Controls.Add(this.StorageButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.LogTextBox);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Eruvian v0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        protected internal System.Windows.Forms.RichTextBox LogTextBox;
+        protected internal System.Windows.Forms.Button StartButton;
+        protected internal System.Windows.Forms.Button PauseButton;
+        protected internal System.Windows.Forms.Button StopButton;
+        protected internal System.Windows.Forms.Button StorageButton;
+        protected internal System.Windows.Forms.Label GoldLabel;
+        protected internal System.Windows.Forms.Label label1;
     }
 }
 
